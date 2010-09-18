e5b8ba26d5f6b29cd27c6cac27e56ce14a1df85e
Chris Johnson <chris@clickonchris.com>
2010-09-18 19:55:12 +0000

    creating iframe

---
+++ test/unit/contestant_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class ContestantTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "math" do
+    assert 1+1 == 2
+    
+    assert 1/(1+1) == 0.5
+  end
+end
