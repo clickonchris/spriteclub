6109199ecf4f88cc6d64af7b3b817fc52d8b4f88
Chris Johnson <chris@clickonchris.com>
2010-12-30 23:24:59 +0000

    testing cookie authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,21 +35,33 @@ class ApplicationController < ActionController::Base
     if facebook_params[:user_id]     
       logger.info "User succesfully authenticated. expires: " + facebook_params[:expires].to_s
       fb_create_user_and_client(facebook_params[:oauth_token], facebook_params[:expires], facebook_params[:user_id])
-      #I don't like having to rely on current_facebook_user here
+      
       @current_user = User.for(facebook_params[:oauth_token], facebook_params[:expires], facebook_params[:user_id])
       session[:facebook_id] = @current_user.facebook_id
+    
+    #Try to authenticate from the session
     elsif session[:facebook_id]
       @current_user = User.find_by_facebook_id(session[:facebook_id])
       
       #make sure the auth token we have is still valid  (I think facebooker2 provides this somehow already but I can't find it)
-      logger.info "token expires: " + @current_user.access_token_expires.to_s
+      logger.info "session auth.  token expires: " + @current_user.access_token_expires.to_s
       if @current_user.access_token_expires == nil  || (@current_user.access_token_expires != nil && @current_user.access_token_expires.utc < Time.now.utc)
         top_redirect_to auth_url
       end
       
       fb_create_user_and_client(@current_user.access_token, @current_user.access_token_expires, @current_user.facebook_id)
+    
+    #Next try to authenticate from a cookie  
+    elsif (hash_data = fb_cookie_hash_for_app_id(app_id)) and
+          fb_cookie_signature_correct?(fb_cookie_hash_for_app_id(app_id),Facebooker2.secret) 
+      logger.info "cookie auth.  token expires: " + hash_data["expires"]
+      @current_user = User.find_by_facebook_id(hash_data["uid"])
+      fb_create_user_and_client(hash_data["access_token"],hash_data["expires"],hash_data["uid"])
       
-    #elsif "cookie found"  #A fbs_<app_id> cookie is another way to check for a user.  implement me!
+      #make sure the auth token is still valid
+      if @current_user.access_token_expires == nil  || (@current_user.access_token_expires != nil && @current_user.access_token_expires.utc < Time.now.utc)
+        top_redirect_to auth_url
+      end
     else
       logger.info "no auth token, session, or cookie found."
       top_redirect_to auth_url
